56d9618ad101256ef3214af1b2e02f6ab6cc35c9
Miguel Fontes <miguel_m_fontes@hotmail.com>
2017-01-25 12:38:25 -0200

    Immovable Interface

--- app/immovables/shared/immovable.interface.ts
+++ app/immovables/shared/immovable.interface.ts
@@ -1,7 +1,23 @@
+export interface Place {
+    address: string;
+}
+
+export interface Image {
+    desc: string;
+    url: string;
+}
+
 export interface Immovable {
-    id: number,
-    name: string,
-    profileIconId: number,
-    summonerLevel: number,
-    revisionDate: number
+    id: number;
+    name: string;
+    description: string;
+    type: string;
+    value: string;
+    place: Place[];
+    images: Image[];
+    contact: string;
+}
+
+export interface RootObject {
+    result: Immovable[];
 }
